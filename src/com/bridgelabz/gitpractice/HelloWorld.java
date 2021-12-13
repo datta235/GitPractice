81969a5835d3e5f71c3f866fa02fa1940148bb6e
datta235 <saidatta1432015@gmail.com>
2021-12-13 09:44:57 +0530

    ADDED: functionality to print name

--- src/com/bridgelabz/gitpractice/HelloWorld.java
+++ src/com/bridgelabz/gitpractice/HelloWorld.java
@@ -2,9 +2,15 @@ package com.bridgelabz.gitpractice;
 
 public class HelloWorld
 {
+	public static final String NAME = "Datta";
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to git practice");
+		printName(NAME);
+	}
+	
+	public static void printName(String name) {
+		System.out.println("My name is : " + name);
 	}
 
 }
